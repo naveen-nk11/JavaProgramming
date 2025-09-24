67ba1f09fc8cf0d505e1dbfc3a886ed94cc497da
naveen-nk11 <naveenkumar090605@gmail.com>
2025-09-24 21:07:04 +0530

    Find Perfect Square

---
+++ FirstWeek/PerfectSquare.java
@@ -0,0 +1,11 @@
+package FirstWeek;
+
+import java.util.Scanner;
+
+public class PerfectSquare {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		
+	}
+}
